03389904d7215718c3dd30898491bc19f43febd6
Vitalii Shutov <vitaly.shutov1@gmail.com>
2026-04-16 18:52:09 +0000

    build: cmake skeleton with CPM bootstrap and interface target

---
+++ cmake/GTestDep.cmake
@@ -0,0 +1,7 @@
+cpmaddpackage(
+    NAME googletest
+    GITHUB_REPOSITORY google/googletest
+    GIT_TAG b514bdc898e2951020cbdca1304b75f5950d1f59
+    SYSTEM YES
+    OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt ON"
+)
